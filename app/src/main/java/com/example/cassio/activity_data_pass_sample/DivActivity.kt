957e26314c92b2b102c31b3c09619b8c958cec18
Cássio Sales <cassio.terceiro@gmail.com>
2018-04-27 23:17:38 -0300

    Add 'back to start' button to last activity

--- app/src/main/java/com/example/cassio/activity_data_pass_sample/DivActivity.kt
+++ app/src/main/java/com/example/cassio/activity_data_pass_sample/DivActivity.kt
@@ -1,7 +1,9 @@
 package com.example.cassio.activity_data_pass_sample
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class DivActivity : AppCompatActivity() {
@@ -15,5 +17,12 @@ class DivActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.textView2).apply {
             text = (values[0] / values[1]).toString()
         }
+    }
+
+    fun backToMain(view : View) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+        finish()
     }
 }
